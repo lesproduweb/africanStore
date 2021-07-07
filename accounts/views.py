49b3e75722283d6d446c61aaa01e755f1aea352e
Jessy Pango <kvnjessy@gmail.com>
2021-07-07 12:47:09 +0100

    11 - Associate Billing Profile to Order

--- accounts/views.py
+++ accounts/views.py
@@ -39,6 +39,13 @@ def login_page(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            try:
+                print("Ici")
+                print(request.session['guest_email_id'])
+                del request.session['guest_email_id']
+                print(request.session['guest_email_id'])
+            except:
+                pass
             if is_safe_url(redirect_path, request.get_host()):
                 return redirect(redirect_path)
             else:
